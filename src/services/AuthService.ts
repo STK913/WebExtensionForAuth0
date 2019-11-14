84dfc38c94732de1fb535ab5abadae37ca35da95
Victor Gillet <victor.gillet@ubimap.fr>
2019-11-14 14:06:17 +0100

    Auth0 updated

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,11 +1,9 @@
 import {Injectable} from '@angular/core';
 import Auth0Lock from 'auth0-lock';
-import Auth0 from 'auth0-js';
 import {Variables} from "../variables";
 
 @Injectable()
 export class AuthService {
-  auth0 = new Auth0.WebAuth({domain: Variables.AUTH0_DOMAIN, clientID: Variables.AUTH0_CLIENT_ID});
   lock = new Auth0Lock(Variables.AUTH0_CLIENT_ID, Variables.AUTH0_DOMAIN, {
     auth: {
       redirect: false,
